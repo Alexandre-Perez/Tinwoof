8e6cfdc609fdb10812f785f7f3f4e4f61c5519c3
Yassin Fekih <yassin.fekih@free.fr>
2022-08-30 14:29:26 +0200

    exo en cours

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,28 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def show
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.user = current_user
+    if @dog.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:gender, :age, :race, :height, :name, :description)
+  end
 end
