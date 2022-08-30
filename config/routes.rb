a036ea558e6fab2ea025bac57da4a2ba210ab5f5
Yassin Fekih <yassin.fekih@free.fr>
2022-08-30 11:55:30 +0200

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users do
+    resources :dogs, only: [:new, :create]
+    resources :reviews
+  end
+  resources :dogs, except: [:new, :create]
+  resources :chatrooms, only: [:show] do
+    resources :messages, only: [:new, :create, :destroy]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_082546) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_094139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_082546) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
