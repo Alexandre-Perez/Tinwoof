0923a369450a3ac6a9bc9be574c9565a7619410b
Yassin Fekih <yassin.fekih@free.fr>
2022-09-01 11:25:59 +0200

    en cours

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,5 @@ class User < ApplicationRecord
   has_many :messages
   validates :first_name, presence: true
   validates :last_name, presence: true
-  #validates :address, presence: true
   has_one_attached :photo
 end
