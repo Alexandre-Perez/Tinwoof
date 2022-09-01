9377c696374a8bccb2293afc500933acdf58415d
alexandre perez <alexandre.perez.egido@gmail.com>
2022-09-01 16:59:21 +0200

    front footer retravailler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-     #@users = User.all
+    @users = User.all
     # @user = current_user
     # @super = request.ip
     # @user.ip = @super
